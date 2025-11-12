000534283c43fcd4efa2bd9ac1b035eeaa0097b8
Helio Henrique Navarro <heliohenriquenavarro@gmail.com>
2025-11-12 20:27:28 +0000

    af

--- af_sishard_25_2/q1/q1.c
+++ af_sishard_25_2/q1/q1.c
@@ -7,15 +7,24 @@
 #include <semaphore.h>
 
 // SÓ PODE USAR A ESTRUTURA ABAIXO, NÃO PODE USAR VARIÁVEIS GLOBAIS
-struct semaforos {
-    // Declare aqui os campos da sua sua estrutura para passar como argumento para as threads
-};
+typedef struct  {
+    sem_t *s1, *s2, *s3, *s4;
+} t_arg;
 
 void *thread1(void *_arg)
 {
+    t_arg *args = (t_arg*) _arg;
 
+    sem_wait(args->s1);
+    sem_wait(args->s2);
     printf("C\n");
+    sem_post(args->s3);
+    sem_post(args->s3);
+    sem_post(args->s3);
 
+
+    sem_wait(args->s3);
+    sem_wait(args->s4);
     printf("E\n");
 
     return NULL;
@@ -23,17 +32,28 @@ void *thread1(void *_arg)
 
 void *thread2(void *_arg)
 {
+    t_arg *args = (t_arg*) _arg;
 
+    sem_wait(args->s1);
+    sem_wait(args->s2);
     printf("D\n");
+    sem_post(args->s4);
+    sem_post(args->s4);
+    sem_post(args->s4);
 
     return NULL;
 }
 
 void *thread3(void *_arg)
 {
+    t_arg *args = (t_arg*) _arg;
 
     printf("A\n");
+    sem_post(args->s1);
+    sem_post(args->s1);
 
+    sem_wait(args->s3);
+    sem_wait(args->s4);
     printf("F\n");
 
     return NULL;
@@ -41,9 +61,14 @@ void *thread3(void *_arg)
 
 void *thread4(void *_arg)
 {
+    t_arg *args = (t_arg*) _arg;
 
     printf("B\n");
+    sem_post(args->s2);
+    sem_post(args->s2);
 
+    sem_wait(args->s3);
+    sem_wait(args->s4);
     printf("G\n");
 
     return NULL;
@@ -52,9 +77,33 @@ void *thread4(void *_arg)
 int main(int argc, char *argv[])
 {
 
-    // Crie TODAS as threads. Voce deve utilizar semaforos para sincronizacao.
+    pthread_t tids[4];
+    void* threads[4] = {thread1, thread2, thread3, thread4};
 
+    sem_t s1,s2,s3,s4;
+    sem_init(&s1,0,0);
+    sem_init(&s2,0,0);
+    sem_init(&s3,0,0);
+    sem_init(&s4,0,0);
+
+    t_arg args = {
+        .s1 = &s1,
+        .s2 = &s2,
+        .s3 = &s3,
+        .s4 = &s4,
+    };
+
+    for (int i = 0; i < 4; i++)
+    {
+        pthread_create(&tids[i], NULL, threads[i], &args);
+    }
+
+    
     // Espere por TODAS as threads
-
+    for (int i = 0; i < 4; i++)
+    {
+        pthread_join(tids[i], NULL);
+    }
+    
     return 0;
 }
